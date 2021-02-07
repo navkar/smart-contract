5ebce003a0582f0a1b1153f7b52e1e5f9e94f5e8
Naveen Karamchetti <navkar@outlook.com>
2021-02-06 19:35:04 -0600

    Moved Mocha to README

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,35 +4,3 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // web3 = instance
 const web3 = new Web3(ganache.provider());
-// Mocha - Testing Framework 
-
-
-class Car {
-    park(){
-        return 'parked';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
-
-beforeEach(() => {
-    car = new Car();
-});
-
-describe('Naveen-Car' ,() => {
-
-    it('can park', () => {
-        assert.strictEqual(car.park(), 'parked');
-        //assert.strictEqual(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.strictEqual(car.drive(), 'vroom');
-        //assert.strictEqual(car.park(), 'stopped');
-    });
-
-});
